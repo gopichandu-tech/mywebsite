74f4f69715601db98b09dfe59a5b7f86d74ac4ce
gopichandu-tech <70055677+gopichandu-tech@users.noreply.github.com>
2023-06-28 18:43:52 +0530

    projects updated

--- src/Components1/Cards/Projects.js
+++ src/Components1/Cards/Projects.js
@@ -1,9 +1,20 @@
 import React from 'react'
-import '../../App.css'
+import './Projects.css'
+import restCountries from './Images/restcountries.jpg'
 
 function Projects() {
   return (
-    <div className='projects'>Projects Will Be Updated Soon...</div>
+    <div className='projects'>
+      <div className='rest-countries'>
+        <a href='https://rest-country-six.vercel.app/'><img src={restCountries} alt='restCountries'/></a>
+        <div className='links'>
+          <div id='title'><p>RestCountries</p></div>
+          <div id='github'><a href='https://github.com/gopichandu-tech/RestCountry'>Github</a></div>
+        </div>
+        <div id='discreption'><p>This project will give basic data (area, region of countries) across the entire world. The data is fetched from the Rest Countries API.</p></div>
+      </div>
+    </div>
+    
   )
 }
 
