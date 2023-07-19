9a0b510dc69990faf4093b1f7e2faa4d9d319bed
gopichandu-tech <70055677+gopichandu-tech@users.noreply.github.com>
2023-07-19 20:52:36 +0530

    estimated_power_check added

--- src/Components1/Cards/Projects.js
+++ src/Components1/Cards/Projects.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Projects.css'
 import restCountries from './Images/restcountries.jpg'
+import EPC from './Images/estimated_power_check.png'
 
 function Projects() {
   return (
@@ -12,6 +13,15 @@ function Projects() {
           <div id='github'><a href='https://github.com/gopichandu-tech/RestCountry'>Github</a></div>
         </div>
         <div id='discreption'><p>This project will give basic data (area, region of countries) across the  entire  world. The data is fetched from the Rest Countries API.</p></div>
+      </div>
+
+      <div className='rest-countries'>
+        <a href='https://github.com/gopichandu-tech/power_consumption_py'><img src={EPC} alt='restCountries'/></a>
+        <div className='links'>
+          <div id='title'><p>Estimated Power consumtion</p></div>
+          <div id='github'><a href='https://github.com/gopichandu-tech/power_consumption_py'>Github</a></div>
+        </div>
+        <div id='discreption'><p>This project  helps us to find the estimated power consumption of an specific software application which is running behind your device.</p></div>
       </div>
     </div>
     
