658fff5ccd8dfb0b24e6db9cdecef1236f2fe7c3
gopichandu-tech <70055677+gopichandu-tech@users.noreply.github.com>
2023-08-10 17:51:15 +0530

    react-typical added to herosection discription

--- src/Components1/Navbar/HeroSection.js
+++ src/Components1/Navbar/HeroSection.js
@@ -1,22 +1,24 @@
 import React from 'react'
 import './HeroSection.css';
+import Typical from 'react-typical';
 function HeroSection() {
   return (
     <section id='hero'>
 
         <div className='container'>
             <div className='info'>
-                <h1>
-                  Hi,
-                  I am Gopi Chandu
+                <h1 className='title'>
+                  Hello,I am Gopi Chandu
                 </h1>
-                <p>
+                <p >
+                  <Typical 
+                   className='discription'
+                  steps={["A Frontend Developer and Data Science enthusiast with keen experience in designing and maintaining responsive websites as well as data analysis in the modern workplace.",1000]}
+                  loop={1}
+                  wrapper='p'
+                />
                     
-                  I'm an junior front end developer studying Btech
-                  Major in Mechanical Engineering and Minor in Computer Science Engineering from
-                  Christ Deemed To Be University Kengeri.
-                  Experience in bulding responsive websites in the recruiting industry.
-                  Proficient  HTML, CSS, JavaScript; plus modern libraries and frameworks.
+                  
                 </p>
             </div>
         </div>
