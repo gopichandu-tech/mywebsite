c022f96f902b67eac3674ac1cd9f228af984a2a8
gopichandu-tech <70055677+gopichandu-tech@users.noreply.github.com>
2023-08-16 19:17:01 +0530

    added movie_recommendation project

--- src/Components1/Cards/Projects/PythonProjects.js
+++ src/Components1/Cards/Projects/PythonProjects.js
@@ -3,12 +3,22 @@ import './PythonProjects.css'
 import project1 from '../Images/python_project1.png'
 import ML1 from '../Images/ml1.png'
 import ML2 from '../Images/ml2.png'
+import ML3 from '../Images/ml3.png'
 
 function PythonProjects() {
   return (
     <div>
-      <h1 className='python-project-heading'>Machine Learning and Python</h1>
+      <h1 className='python-project-heading'>Machine Learning </h1>
       <div className='projects'>
+      <div className='project Movies Recommendation'>
+            <a href='https://github.com/gopichandu-tech/moviesrecommendation_machineLearning'><img src={ML3} alt='restCountries'/></a>
+            <div className='links'>
+            <div id='title'><p>Movies Recommendation</p></div>
+            <div id='github'><a href='https://github.com/gopichandu-tech/moviesrecommendation_machineLearning'>Github</a></div>
+            </div>
+            <div id='discreption'><p>Top 10000 movies from the TMDB. Includes information such as release year, genre, original language, vote average and count, popularity, and budget.</p></div>
+      </div>
+
       <div className='project Domestic violence in Colombia'>
             <a href='https://github.com/gopichandu-tech/Domestic-violence-in-Columbia'><img src={ML1} alt='restCountries'/></a>
             <div className='links'>
